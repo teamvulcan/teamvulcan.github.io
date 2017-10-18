c4e5f1372dc63035bc8d3c5d9fb1479908b9f1eb
MaxHolzmann <max@holzmann.io>
2017-10-17 21:25:53 -0500

    some stuff

---
+++ calc.js
@@ -0,0 +1,10 @@
+function calculate() {
+  var one = document.getElementById('first').value;
+  var two = document.getElementById('second').value;
+  var operator = document.getElementById('op').value;
+  var answer = document.getElementById('ans');
+  var final = one + two;
+
+    ans.textContent = final;
+
+}
--- castle.js
+++ castle.js
@@ -1,16 +1,14 @@
 //castle crashers tool js scripts
-
-function picking(){
-var charput = document.getElementById('charinput').value;
+var charput = document.getElementById('charput');
+var charputvalue = document.getElementById('charinput').value;
+var charimg = document.getElementById('charpic');
 var title = document.getElementById('chartitle');
 var info = document.getElementById('charinfo');
-var castlepic = document.getElementById('charpic');
 
-switch(charput) {
-case 'red':
-title.innerHTML = "<h2>Red Knight</h2>";
-info.innerHTML = "This is the red knight.";
-castlepic.innerHTML ="<div class='row'><div class='col-md-4'><div class='thumbnail'> <img src='images/castle/red.png'></img> </div></div>";
-break;
+function getCharacter(name){
+ this.name = charputvalue;
 }
+
+function update() {
+
 }
