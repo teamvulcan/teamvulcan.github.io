c4b1eb8b4e13bb72c9f626be12aa849c0992fbd3
Max Holzmann <max@holzmann.io>
2017-10-25 18:45:08 +0000

    end of day

--- castle.js
+++ castle.js
@@ -13,7 +13,9 @@ function first(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-var red = new castleChar("Red", "Description", "image");
+//variables for each character
+var red = new castleChar("Red Knight", "Description", "image", "Mace", "Electricity", "Default");
+var green = new castleChar("Green Knight", "Description", "image", "Thin Sword", "Poison", "Default");
 
 // function to update the html page
 function update() {
@@ -22,6 +24,9 @@ var charvalue = document.getElementById('castleinput').value;
 var charpic = document.getElementById('charpic');
 var title = document.getElementById('chartitle');
 var castlethumb = document.getElementById('castlethumb');
+var castleweapon = document.getElementById('startweapon');
+var castlemagic = document.getElementById('magic');
+var castleunlock = document.getElementById('unlock');
 title.textContent = first(charvalue); //sets title to the name of the picked character
 castlethumb.style.display = "initial";
 switch(charvalue) { //sorts between character values
@@ -32,6 +37,15 @@ switch(charvalue) { //sorts between character values
   break;
   case 'Red Knight':
   charpic.src='images/castle/red.png';
+  castleweapon.textContent = red.startweapon;
+  castlemagic.textContent = red.magic;
+  castleunlock.textContent = red.unlock;
+  break;
+  case 'Green Knight':
+  charpic.src= 'images/castle/green.png';
+  castleweapon.textContent = green.startweapon;
+  castlemagic.textContent = green.magic;
+  castleunlock.textContent = green.unlock;
   break;
 }
 
